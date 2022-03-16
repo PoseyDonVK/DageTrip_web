53ae2e4a58d0ba6cfe5341f1f4f5ec456e00ffbc
Ibra <alibekov.ibragim@bk.ru>
2022-03-17 02:24:46 +0300

    first commit

--- travel/travel_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Route)
-admin.site.register(Category)
-admin.site.register(Place)
--- travel/travel_app/views.py
+++ travel/travel_app/views.py
@@ -1,3 +1,14 @@
+from django.contrib.contenttypes import views
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+def mainpage(request):
+    msg = request.GET['name']
+    msg2 = request.GET['message']
+    return render(request, 'travel_app/anotherpage.html', {'name': msg, 'message': msg2})
+
+def welcome(request):
+    return redirect('/hello?name=Recruto&message=Давай+дружить')
